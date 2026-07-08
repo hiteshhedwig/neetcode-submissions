26c56d8ca0f3d608188c04f5e9e9824bd3bf172e
Hitesh Kumar <40434383+hiteshhedwig@users.noreply.github.com>
2026-07-08 22:45:38 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==1:
+            return 1
+        
+        prev1 = 2
+        prev2 = 1
+
+        for i in range(2, n):
+            current=prev2 + prev1
+            prev2 = prev1
+            prev1 = current
+        return prev1
